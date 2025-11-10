d29ac7614671731fd1c8b77cd6f22efa60928891
Manu-2-6 <emmanuelamix@hotmail.com>
2025-11-10 11:04:22 +0100

    Add requested driver and GPU applications

--- SoftwareSetupApp/MainWindow.xaml.cs
+++ SoftwareSetupApp/MainWindow.xaml.cs
@@ -585,6 +585,9 @@ if ($target -ne [IntPtr]::Zero) {
         Packages.Add(new SoftwarePackage("Mozilla Firefox", "Mozilla.Firefox"));
         Packages.Add(new SoftwarePackage("Adobe Acrobat Reader", "Adobe.Acrobat.Reader.64-bit"));
         Packages.Add(new SoftwarePackage("CPU-Z", "CPUID.CPU-Z"));
+        Packages.Add(new SoftwarePackage("Driver Booster", "IObit.DriverBooster"));
+        Packages.Add(new SoftwarePackage("Nvidia App", "NVIDIA.App"));
+        Packages.Add(new SoftwarePackage("AMD Software Adrenalin", "AdvancedMicroDevices.AMDSoftware"));
 
         foreach (var package in Packages)
         {
